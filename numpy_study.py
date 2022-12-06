44cd20c01149bf3b9f27e7224134fd7beab07b11
justicefull <chani9793@naver.com>
2022-12-06 19:21:19 +0900

    Numpy 기초 내용 학습

---
+++ numpy_study.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+# 넘파이 배열 생성하기
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+type(x)
+
+# 넘파이 산술 연산
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+# 넘파이의 N차원 배열
+A = np.array([[1, 2], [3, 4]]) # 2 X 2 행렬
+print(A)
+print(A.shape)
+print(A.dtype)
+
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+print(A * B)
+
+print(A * 10)
+
+# 브로드캐스트
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
+
+# 원소 접근
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+print(X[0])
+print(X[0][1])
+
+for row in X:
+    print(row)
+
+X = X.flatten()
+print(X)
+print(X[np.array([0, 2, 4])])
