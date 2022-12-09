6a6b9a39f06a4bbf3e8941ffbd79c558a538dc0d
justicefull <chani9793@naver.com>
2022-12-09 18:02:27 +0900

    소프트맥스 함수 구현, 함수 그래프 주석 처리(import시 그래프가 실행됨, 원인 모르겠음)

--- chapter3_neural_network/activation_function.py
+++ chapter3_neural_network/activation_function.py
@@ -6,6 +6,7 @@ def step_function(x):
     y = x > 0
     return y.astype(np.int) # 자료형 변환을 하지 않으면 y값이 불리언 값으로 생성됨
 
+"""
 # 계단 함수 그래프
 def step_function(x):
     return np.array(x > 0, dtype=np.int64) # x가 0보다 크다면 숫자형으로 배열을 리턴
@@ -15,6 +16,7 @@ y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1) # y축 범위 지정
 plt.show()
+"""
 
 # 시그모이드 함수 구현
 def sigmoid(x):
@@ -23,20 +25,44 @@ def sigmoid(x):
 x = np.array([-1.0, 1.0, 2.0])
 print(sigmoid(x))
 
+"""
 # 시그모이드 함수 그래프
 x = np.arange(-5.0, 5.0, 0.1)
 y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1) # y축 범위 지정
 plt.show()
+"""
 
 # ReLU 함수 구현
 def relu(x):
     return np.maximum(0, x) # maximum은 두 입력 중 큰 값을 선택하여 반환하는 함수
 
+"""
 # ReLU 함수 그래프
 x = np.arange(-5.0, 5.0, 0.1)
 y = relu(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 5) # y축 범위 지정
 plt.show()
+"""
+
+# 소프트맥스 함수 구현
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+"""
+위의 구현은 오버플로의 문제가 발생할 수 있는 함수 구현이다.
+지수 함수가 큰 값을 반환하여 큰 값끼리 나눗셈을 하게 되면 결과 수치가 불안정해진다.
+"""
+
+# 소프트맥스 함수 구현(개선)
+def softmax(a):
+    c = np.max(a) # a로 받게될 값 중 최댓값을 c로 할당
+    exp_a = np.exp(a - c) # 최댓값에서 a값을 뺴주어 수의 크기를 줄여주어 오버플로 문제를 방지
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
