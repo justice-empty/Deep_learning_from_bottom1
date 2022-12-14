1f1ae4766128b766dc0e4b36c681f9dbcd978006
justicefull <chani9793@naver.com>
2022-12-14 19:02:15 +0900

    파일 통합

--- chapter4_neural_network_training/numerical_gradient.py
+++ chapter4_neural_network_training/numerical_gradient.py
@@ -38,3 +38,28 @@ def function_tmp2(x1):
     return 3.0**2.0 + x1*x1
 
 print(numerical_diff(function_tmp2, 4.0))
+
+# 기울기 구현
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x) # x와 같은 형상의 0으로된 배열 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+
+    return grad
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
