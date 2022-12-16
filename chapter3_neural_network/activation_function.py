9933adfd6e3ffa6b94066c4e13ac57864e3172cc
justicefull <chani9793@naver.com>
2022-12-16 18:31:21 +0900

    import 시 발생하는 문제 해결(이슈2번)

--- chapter3_neural_network/activation_function.py
+++ chapter3_neural_network/activation_function.py
@@ -7,45 +7,22 @@ def step_function(x):
     return y.astype(np.int) # 자료형 변환을 하지 않으면 y값이 불리언 값으로 생성됨
 
 """
-# 계단 함수 그래프
 def step_function(x):
     return np.array(x > 0, dtype=np.int64) # x가 0보다 크다면 숫자형으로 배열을 리턴
-
-x = np.arange(-5.0, 5.0, 0.1) # -5부터 5미만 까지 0.1의 간격으로 배열 생성
-y = step_function(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1) # y축 범위 지정
-plt.show()
 """
 
 # 시그모이드 함수 구현
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-x = np.array([-1.0, 1.0, 2.0])
-
-"""
-# 시그모이드 함수 그래프
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1) # y축 범위 지정
-plt.show()
-"""
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
 
 # ReLU 함수 구현
 def relu(x):
     return np.maximum(0, x) # maximum은 두 입력 중 큰 값을 선택하여 반환하는 함수
-
+    
 """
-# ReLU 함수 그래프
-x = np.arange(-5.0, 5.0, 0.1)
-y = relu(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 5) # y축 범위 지정
-plt.show()
-"""
-
 # 소프트맥스 함수 구현
 def softmax(a):
     exp_a = np.exp(a)
@@ -53,7 +30,6 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y
 
-"""
 위의 구현은 오버플로의 문제가 발생할 수 있는 함수 구현이다.
 지수 함수가 큰 값을 반환하여 큰 값끼리 나눗셈을 하게 되면 결과 수치가 불안정해진다.
 """
@@ -65,3 +41,26 @@ def softmax(a):
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
+if __name__ == '__main__':
+
+    # 계단 함수 그래프
+    x = np.arange(-5.0, 5.0, 0.1) # -5부터 5미만 까지 0.1의 간격으로 배열 생성
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1) # y축 범위 지정
+    plt.show()
+
+    # 시그모이드 함수 그래프
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = sigmoid(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1) # y축 범위 지정
+    plt.show()
+    
+    # ReLU 함수 그래프
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = relu(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 5) # y축 범위 지정
+    plt.show()
