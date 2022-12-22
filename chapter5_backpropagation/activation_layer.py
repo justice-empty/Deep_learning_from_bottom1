04869af2bb4d8a5d57b41b84258207d95842ab4d
justicefull <chani9793@naver.com>
2022-12-22 17:49:50 +0900

    Affine, Softmax-with-Loss 계층 클래스 구현

--- chapter5_backpropagation/activation_layer.py
+++ chapter5_backpropagation/activation_layer.py
@@ -1,4 +1,6 @@
 import numpy as np
+from chapter3_neural_network.activation_function import softmax
+from chapter4_neural_network_training.loss_function import cross_entropy_error
 
 class Relu:
     def __init__(self):
@@ -29,5 +31,45 @@ class Sigmoid:
     
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dw = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SofrmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 손실
+        self.y = None # softmax의 출력
+        self.t = None # 정답 레이블
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
