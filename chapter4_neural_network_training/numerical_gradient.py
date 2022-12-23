d7d5fc0d68e4d91b7c821761184937c4df9f58d6
justicefull <chani9793@naver.com>
2022-12-23 23:14:12 +0900

    수치 미분으로 기울기 구하는 함수 개선하여 추가

--- chapter4_neural_network_training/numerical_gradient.py
+++ chapter4_neural_network_training/numerical_gradient.py
@@ -34,25 +34,37 @@ def function_tmp2(x1):
 
 # 기울기 구현
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x) # x와 같은 형상의 0으로된 배열 생성
-
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+    
     for idx in range(x.size):
         tmp_val = x[idx]
-
+        
         # f(x+h) 계산
-        x[idx] = tmp_val + h
+        x[idx] = float(tmp_val) + h
         fxh1 = f(x)
-
+        
         # f(x-h) 계산
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) 
+        
         grad[idx] = (fxh1 - fxh2) / (2*h)
         x[idx] = tmp_val # 값 복원
-
+        
     return grad
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+        
+        return grad
 
 # 경사법(경사하강법) 구현
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
