0e2d5a0a85e8108f4cbe006a6e67cae0c8a1d99f
justicefull <chani9793@naver.com>
2022-12-23 23:15:00 +0900

    오타 수정

--- chapter5_backpropagation/two_layer_net.py
+++ chapter5_backpropagation/two_layer_net.py
@@ -14,15 +14,15 @@ class TwoLayerNet:
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(input_size, hidden_size)
-        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
         # 계층 생성
         self.layers = OrderedDict()
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
         self.layers['Relu1'] = Relu()
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
-        self.lastLayer = SofrmaxWithLoss()
+        self.lastLayer = SoftmaxWithLoss()
 
     def predict(self, x):
         for layer in self.layers.values():
@@ -31,11 +31,11 @@ class TwoLayerNet:
         return x
     
     def loss(self, x, t):
-        y =self.predict(x)
+        y = self.predict(x)
 
         return self.lastLayer.forward(y, t)
 
-    def accuracy(self, x ,t):
+    def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
         if t.ndim != 1 : t = np.argmax(t, axis=1)
@@ -70,9 +70,9 @@ class TwoLayerNet:
 
         # 결과 저장
         grads = {}
-        grads['W1'] = self.layers['Affine1'].dw
+        grads['W1'] = self.layers['Affine1'].dW
         grads['b1'] = self.layers['Affine1'].db
-        grads['W2'] = self.layers['Affine2'].dw
+        grads['W2'] = self.layers['Affine2'].dW
         grads['b2'] = self.layers['Affine2'].db
 
         return grads
