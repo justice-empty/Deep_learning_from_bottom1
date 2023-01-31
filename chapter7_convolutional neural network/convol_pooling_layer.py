0de8eae70814bb967ac4958dbfc072b9615dc1bf
justicefull <chani9793@naver.com>
2023-01-31 17:37:46 +0900

    합성곱, 풀링 계층 구현 (역전파 미구현)

--- chapter7_convolutional neural network/convol_pooling_layer.py
+++ chapter7_convolutional neural network/convol_pooling_layer.py
@@ -23,7 +23,31 @@ class Convolution:
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
         return out
+        
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
 
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        # 1. 전개
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        # 2. 최댓값
+        out = np.max(col, axis=1)
+
+        # 3. 성형
+        out = out.reshape(N, out_h, out_w, C).trasnpose(0, 3, 1, 2)
+
+        return out
+        
 if __name__ == '__main__':
     x1 = np.random.rand(1, 3, 7, 7) # (데이터 수, 채널 수, 높이, 너비)
     col1 = im2col(x1, 5, 5, stride=1, pad=0)
